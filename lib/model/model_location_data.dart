c0a444df06be2331fac084cb1b6021097a4237d0
darshil.gabani <darshil.g@biztechcs.com>
2024-01-12 14:57:32 +0530

    model_location_data changed for WorkDone

--- lib/model/model_location_data.dart
+++ lib/model/model_location_data.dart
@@ -2,6 +2,8 @@ class LocationDataModel {
   final String? markerId;
   final String? locationTag;
   final String? latLng;
+  final bool? isWorkedDone;
 
-  LocationDataModel(this.markerId, this.locationTag, this.latLng);
+  LocationDataModel(
+      this.markerId, this.locationTag, this.latLng, this.isWorkedDone);
 }
